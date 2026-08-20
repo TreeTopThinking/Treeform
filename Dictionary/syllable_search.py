533ad54d109bbd51ed2d11117113939a5ccaad49
Jason Cooper <jason@Jasons-MacBook-Air.local>
2026-08-20 20:27:02 +0200

    Reformating the markdown files

---
+++ Dictionary/syllable_search.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import random
+
+letters = ['i', 'u', 'a', 'o', 'q', 'e', 'y', 'm', 'n', 'p', 'b', 't', 'd', 'k', 'g', 'l', 'w', 'r', 'v', 'z', 'j', 's', 'f']
+
+df = pd.read_csv("syllables.csv")
+
+def check_next_to(syllable):
+    for i in range(len(syllable) - 1):
+        if syllable[i] == syllable[i + 1]:
+            return True
+    return False
+
+length = random.choice([2])
+syllable = "".join(random.choices(letters, k=length))
+
+while syllable in df["whorlform"].values or check_next_to(syllable):
+    length = random.choice([2])
+    syllable = "".join(random.choices(letters, k=length))
+
+print(syllable)
